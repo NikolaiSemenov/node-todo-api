a73c86445974f66d4507be242decea44490e9729
Nikolai Semenov <nikolai.a.semenov@gmail.com>
2017-07-22 19:11:29 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mogoose} = require('./db/mogoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,23 @@ app.get('/todos', (req, res) => {
 		res.status(400).send(e);
 	})
 })
+
+// GET /todos/12345
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}, (e) => {
+		res.status(400).send(e);
+	})
+});
+
 
 app.listen(3000, () => {
 	console.log('Started om port 3000');
