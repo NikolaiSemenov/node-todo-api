40b20dd29a15ba9a56af618de4457826ea9e5191
Nikolai Semenov <nikolai.a.semenov@gmail.com>
2017-07-22 13:00:55 +0700

    Add update script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -10,9 +10,19 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	}
 	console.log('Connected to MongoDB server');
 
-	db.collection('Todos').insertOne({
-		text: 'Something to do',
-		completed: false
+	// db.collection('Todos').insertOne({
+	// 	text: 'Something to do',
+	// 	completed: false
+	// }, (err, result) => {
+	// 	if (err) {
+	// 		return console.log('Unable to insert todo', err)
+	// 	}
+	// 	console.log(JSON.stringify(result.ops, undefined, 2));
+	// });
+	db.collection('Users').insertOne({
+		name: 'Kirill',
+		age: 32,
+		location: 'Vladivostok'
 	}, (err, result) => {
 		if (err) {
 			return console.log('Unable to insert todo', err)
