f7bf3891b7fd7663cf53ad7344198357f7fc730f
Nikolai Semenov <nikolai.a.semenov@gmail.com>
2017-07-29 13:49:39 +0700

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -50,6 +50,16 @@ UserSchema.methods.generateAuthToken = function () {
 	});
 };
 
+UserSchema.methods.removeToken = function (token) {
+	var user = this;
+
+	return user.update({
+		$pull: {
+			tokens: {token}
+		}
+	});
+};
+
 UserSchema.statics.FindByToken = function (token) {
 	var User = this;
 	var decoded;
