b764f24cf777736bb27034ba593d7d95b09d64eb
Nikolai Semenov <nikolai.a.semenov@gmail.com>
2017-07-29 17:36:37 +0700

    Associate users with todos

--- server/test/seed/seed.js
+++ server/test/seed/seed.js
@@ -17,17 +17,23 @@ const users = [{
 }, {
 	_id: userTwoId,
 	email: 'nikolai@example.com',
-	password: 'userTwoPassword'
+	password: 'userTwoPassword',
+	tokens: [{
+		access: 'auth',
+		token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+	}]
 }];
 
 const todos = [{
 	_id: new ObjectID(),
-	text: 'First test todo'
+	text: 'First test todo',
+	_creator: userOneId
 }, {
 	_id: new ObjectID(),
 	text: 'Second test todo',
 	completed: true,
-	completedAt: 335
+	completedAt: 335,
+	_creator: userTwoId
 }];
 
 const populateTodos = (done) => {
